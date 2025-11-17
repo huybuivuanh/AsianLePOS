9cb3a6f50ffbf341ce923881e83f0bdb2f5fba77
huybuivuanh <huy_buivu12345@yahoo.ca>
2025-11-17 01:23:15 -0600

    back arrow for web version

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,6 +1,7 @@
 // components/ui/Header.tsx
 import { Ionicons } from "@expo/vector-icons";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ArrowLeft } from "lucide-react-native";
+import { Platform, Text, TouchableOpacity, View } from "react-native";
 
 export default function Header({
   title,
@@ -12,7 +13,11 @@ export default function Header({
   return (
     <View className="flex-row items-center justify-between px-4 py-3 bg-gray-400">
       <TouchableOpacity onPress={onBack}>
-        <Ionicons name="arrow-back" size={40} color="white" />
+        {Platform.OS === "web" ? (
+          <ArrowLeft size={32} color="white" />
+        ) : (
+          <Ionicons name="arrow-back" size={40} color="white" />
+        )}
       </TouchableOpacity>
       <Text className="text-white text-xl font-semibold">{title}</Text>
       {/* spacer */}
