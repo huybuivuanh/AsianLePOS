9e6350c4312b9d682a52c30cbefcf2fe136fde8d
huybuivuanh <huy_buivu12345@yahoo.ca>
2026-03-29 18:49:53 -0600

    remove unused components

--- src/features/takeout/index.ts
+++ src/features/takeout/index.ts
@@ -3,9 +3,7 @@ export {
   getVisibleMenuItemsInCategoryOrder,
 } from "../../utils/menuOrdering";
 export { default as CategoryGrid } from "./components/CategoryGrid";
-export { default as CategoryItem } from "./components/CategoryItem";
 export { default as CategoryItemsView } from "./components/CategoryItemsView";
-export { default as CategoryList } from "./components/CategoryList";
 export { default as CustomerInfoForm } from "./components/CustomerInfoForm";
 export { default as OrderFooter } from "./components/OrderFooter";
 export { default as ReadyTimeSelector } from "./components/ReadyTimeSelector";
